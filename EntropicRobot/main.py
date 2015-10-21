8006fd3ad4df9e946271e82650c0d8debcaa481b
bendaf <bendaf92@gmail.com>
2015-10-21 16:20:01 +0200

    Modify main to race sensor and entropic robot

--- EntropicRobot/main.py
+++ EntropicRobot/main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from PIL import Image  # Python Imaging Library
 from robot import Robot
+from robot_with_sensor import SRobot
 
 pygame.init()
 
@@ -29,21 +30,41 @@ class Environment:
 def draw_pixel(surface, r, g, b, x, y):
         surface.fill((r, g, b), ((x, y), (2, 2)))
 
+def robot_list_step():
+    for r in robot_list:
+        future_positions = r.simulate()
+        r.move()
+        try:
+            for p in future_positions:
+                draw_pixel(screen, 0, 0, 255, *p)
+        except TypeError:
+            pass
+
+def robot_list_draw():
+    for r in robot_list:
+        r.draw(screen)
+        if not r.in_bounds():
+            running = False
+
 environment = Environment()
 
 #  Create screen
 screen = pygame.display.set_mode((environment.get_size()))
 pygame.display.set_caption('Entropic AI')
 background = pygame.image.load(image_filename).convert()  # for display
-velocity_font = pygame.font.Font(None, 30)
+# velocity_font = pygame.font.Font(None, 30)
+robot_list = list()
 
-myRobot = Robot(environment, 560, 90, 7)
+robot_list.append(SRobot(environment, environment.width/2, environment.height/2, 7))
+robot_list.append(Robot(environment,environment.width/2,environment.height/2,7))
 walked_path = list()
 
 clock = pygame.time.Clock()
 running = True
 paused = False
 draw_walked_path = True
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -57,48 +78,45 @@ while running:
             elif event.key == pygame.K_f:
                 draw_walked_path ^= True
             elif event.key == pygame.K_w:
-                myRobot.timeHorizon += 5
-                print("time horizon = ", myRobot.timeHorizon)
+                robot_list[0].timeHorizon += 5
+                print("time horizon = ", robot_list[0].timeHorizon)
             elif event.key == pygame.K_s:
-                myRobot.timeHorizon -= 5
-                print("time horizon = ", myRobot.timeHorizon)
+                robot_list[0].timeHorizon -= 5
+                print("time horizon = ", robot_list[0].timeHorizon)
             elif event.key == pygame.K_d:
-                myRobot.numberOfPaths += 5
-                print("number of paths = ", myRobot.numberOfPaths)
+                robot_list[0].numberOfPaths += 5
+                print("number of paths = ", robot_list[0].numberOfPaths)
             elif event.key == pygame.K_a:
-                myRobot.numberOfPaths -= 5
-                print("number of paths = ", myRobot.numberOfPaths)
+                robot_list[0].numberOfPaths -= 5
+                print("number of paths = ", robot_list[0].numberOfPaths)
             elif event.key == pygame.K_x:
-                myRobot.maxSpeed += 1.0
-                print("max speed = ", myRobot.maxSpeed)
+                robot_list[0].maxSpeed += 1.0
+                print("max speed = ", robot_list[0].maxSpeed)
             elif event.key == pygame.K_z:
-                myRobot.maxSpeed -= 1.0
-                print("max speed = ", myRobot.maxSpeed)
+                robot_list[0].maxSpeed -= 1.0
+                print("max speed = ", robot_list[0].maxSpeed)
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             print(pygame.mouse.get_pos())
-            myRobot.pos.x, myRobot.pos.y = pygame.mouse.get_pos()
-            myRobot.speed = 0
+            robot_list[0].pos.x, robot_list[0].pos.y = pygame.mouse.get_pos()
+            robot_list[0].speed = 0
+            robot_list[1].pos.x, robot_list[1].pos.y = pygame.mouse.get_pos()
+            robot_list[1].speed = 0
             walked_path.clear()
 
-    screen.blit(background, [0, 0])  # redraw clean background
+    # screen.blit(background, [0, 0])  # redraw clean background
     # Update:
     if not paused:
-        myRobot.sensor()
-        future_positions = myRobot.simulate()
-        myRobot.move()
-        walked_path.append(myRobot.pos.as_int())
-        for p in future_positions:
-            draw_pixel(screen, 0, 0, 255, *p)
-    myRobot.draw(screen)
-    text = velocity_font.render("v = "+str(myRobot.speed), 1, (255, 0, 0))
-    screen.blit(text, (0, 0))
+        screen.blit(background, [0, 0])  # redraw clean background
+        robot_list_step()
+        # paused = True
+
+    robot_list_draw()
+    #text = velocity_font.render("v = "+str(myRobot.speed), 1, (255, 0, 0))
+    #screen.blit(text, (0, 0))
 
     if len(walked_path) > 1 and draw_walked_path:
         pygame.draw.lines(screen, (0, 255, 0), False, walked_path, 1)
-
-    if not myRobot.in_bounds():
-        running = False
 
     pygame.display.flip()
     #pygame.time.wait(30)
